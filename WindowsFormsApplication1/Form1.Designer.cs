bc7e086846b156d4d72f9fcaec9fab32156eb84c
Mitsuru Takeuchi <mitsuru@hi-farm.net>
2013-02-27 19:44:57 +0900

    first

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IPTextBox = new System.Windows.Forms.TextBox();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.logtext = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "host";
+            // 
+            // IPTextBox
+            // 
+            this.IPTextBox.Location = new System.Drawing.Point(11, 24);
+            this.IPTextBox.Name = "IPTextBox";
+            this.IPTextBox.Size = new System.Drawing.Size(100, 19);
+            this.IPTextBox.TabIndex = 1;
+            this.IPTextBox.Text = "127.0.0.1";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(129, 24);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(100, 19);
+            this.portTextBox.TabIndex = 2;
+            this.portTextBox.Text = "12000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(11, 50);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 4;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // logtext
+            // 
+            this.logtext.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logtext.Location = new System.Drawing.Point(11, 79);
+            this.logtext.Name = "logtext";
+            this.logtext.Size = new System.Drawing.Size(261, 171);
+            this.logtext.TabIndex = 5;
+            this.logtext.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.logtext);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.IPTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TcpClient sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IPTextBox;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.RichTextBox logtext;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client;
+        private NetworkStream ns;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private string str;
+        private Thread thread;
+
+        public delegate void updateLogDelegate(string newstr);
+        public updateLogDelegate updateLog;
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            updateLog = updateLogProc;
+        }
+
+        void updateLogProc(string newstr) {
+            string text = str + logtext.Text;
+            logtext.AppendText(text);
+        }
+
+        private void readLoop() {
+            while (true) {
+                string res = Read();
+                Console.WriteLine(res);
+                str = res;
+
+                string text = "";
+                this.Invoke(updateLog, str);
+            }
+        }
+
+        private string Read() {
+            string res = "";
+            if (ns.CanRead)
+            {
+                int read = 0;
+                byte[] bytes = new byte[client.ReceiveBufferSize];
+                try
+                {
+                    do
+                    {
+                        read = ns.Read(bytes, 0, client.ReceiveBufferSize);
+                        //res = reader.ReadLine();
+                    } while (ns.DataAvailable);
+                }
+                catch (ObjectDisposedException dispose) { Console.WriteLine("object disposed :" + dispose.Message); }
+                catch (IOException io) { Console.WriteLine("ioexception:" + io.Message); }
+                catch (ArgumentOutOfRangeException argrange) { Console.WriteLine("argument out of :" + argrange.Message); }
+                catch (ArgumentException arg) { Console.WriteLine("argument:" + arg.Message); }
+                
+                res = Encoding.ASCII.GetString(bytes, 0, read);
+            }
+            return res;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+            this.FormClosed += Form1_FormClosed;
+        }
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            client.Close();
+            thread.Join();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            startbutton.Enabled = false;
+            string host = IPTextBox.Text;
+            int port = int.Parse(portTextBox.Text);
+            client = new TcpClient(host, port);
+            ns = client.GetStream();
+            writer = new StreamWriter(ns);
+            reader = new StreamReader(ns);
+            string mes = "aaaaaa";
+            byte[] tmp = Encoding.ASCII.GetBytes(mes);
+            ns.Write(tmp, 0, tmp.Length);
+            thread = new Thread(new ThreadStart(this.readLoop));
+            thread.IsBackground = true;
+            thread.Start();
+            while (!thread.IsAlive) { }
+        }
+    }
+}
